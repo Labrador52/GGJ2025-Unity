e53e9b16b85a0695b7cb1faf01f883731c9f0780
Labrador52 <xuzhuo202004@outlook.com>
2025-01-18 16:03:54 +0800

    FoW manager

---
+++ Assets/Scripts/Bubble/BubbleFogTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleFogTrigger : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Triggered");
+        if (collision.gameObject.tag == "Fog")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
